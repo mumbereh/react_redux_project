c0376127a4a0f02fff0257f1b7fcc1362839f378
mumbereh <siirasonh@gmail.com>
2023-09-12 14:41:54 +0300

    configured linter

--- src/Redux/Home/HomePageSlice.js
+++ src/Redux/Home/HomePageSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const urlBase = 'https://api.coingecko.com/api/v3/coins/';
-const urlFetcherAPI = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en';
+const urlFetcherAPI = 'https://api.coingecko.com/api/v3/coins/';
 
 export const fetchCategories = createAsyncThunk('homepage/fetchCategories', async () => {
   try {
